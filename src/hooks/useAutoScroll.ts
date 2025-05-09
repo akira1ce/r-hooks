3207d09ad6c33efecb0add2ebfbcb04c2037bcf7
akira1ce <akira1ce@163.com>
2025-05-09 12:25:31 +0800

    fix: remove options of useAutoScroll

--- src/hooks/useAutoScroll.ts
+++ src/hooks/useAutoScroll.ts
@@ -1,36 +1,26 @@
-import { RefObject, useEffect, useState } from 'react';
+import { DependencyList, RefObject, useEffect, useState } from 'react';
 
-interface UseAutoScrollOptions {
-  /* 触发自动滚动的依赖项 */
-  dependencies?: any[];
-  /* 判断是否在底部的阈值 */
-  threshold?: number;
-}
-
-export const useAutoScroll = (target: RefObject<Element>, options: UseAutoScrollOptions = {}) => {
-  const { dependencies = [], threshold = 1 } = options;
-
+export const useAutoScroll = (target: RefObject<Element>, deps: DependencyList = []) => {
   const [enableAutoScroll, setEnableAutoScroll] = useState(true);
 
-  /* 滚动到底部 */
+  // 滚动到底部
   const scrollToBottom = () => {
-    if (target.current && enableAutoScroll) {
+    if (target.current) {
       target.current.scrollTop = target.current.scrollHeight;
     }
   };
 
-  /* 处理滚动事件 */
+  // 处理滚动事件
   const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
     const element = e.currentTarget;
-    const isScrolledToBottom =
-      Math.abs(element.scrollHeight - element.scrollTop - element.clientHeight) < threshold;
+    const isScrolledToBottom = Math.abs(element.scrollHeight - element.scrollTop - element.clientHeight) < 1;
     setEnableAutoScroll(isScrolledToBottom);
   };
 
-  /* 监听依赖项变化，自动滚动到底部 */
+  // 监听依赖项变化，自动滚动到底部
   useEffect(() => {
-    scrollToBottom();
-  }, dependencies);
+    enableAutoScroll && scrollToBottom();
+  }, [...deps, enableAutoScroll]);
 
   return {
     handleScroll,
