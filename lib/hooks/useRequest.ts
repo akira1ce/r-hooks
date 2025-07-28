c8d09c313c5bae7cbd0c9500c38e40229a3c2176
akira1ce <akira1ce@163.com>
2025-07-28 17:46:57 +0800

    fix(useRequest): cancel closure in useEffect

--- lib/hooks/useRequest.ts
+++ lib/hooks/useRequest.ts
@@ -1,73 +1,71 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react';
+import { useMemoizedFn } from './useMemoizedFn';
 
 export interface UseRequestOptions {
-	/** whether to manually trigger */
-	manual?: boolean;
-	/** polling interval */
-	pollingInterval?: number;
-	/** polling retry count */
-	pollingRetryCount?: number;
+  /** whether to manually trigger */
+  manual?: boolean;
+  /** polling interval */
+  pollingInterval?: number;
+  /** polling retry count */
+  pollingRetryCount?: number;
 }
 
 export type Service<TData, TParams> = (params?: TParams) => Promise<TData>;
 
 export const useRequest = <TData, TParams>(
-	api: Service<TData, TParams>,
-	options: UseRequestOptions = {},
+  api: Service<TData, TParams>,
+  options: UseRequestOptions = {},
 ) => {
-	const [loading, setLoading] = useState(false);
-	const [data, setData] = useState<TData>();
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<TData>();
 
-	/* polling timer */
-	const timer = useRef<any>(null);
-	const retryCount = useRef(0);
+  /* polling timer */
+  const timer = useRef<any>(null);
+  const retryCount = useRef(0);
 
-	const { manual = false, pollingInterval, pollingRetryCount } = options;
+  const { manual = false, pollingInterval, pollingRetryCount } = options;
 
-	/* cancel polling */
-	const cancel = useCallback(() => {
-		if (timer.current) {
-			clearTimeout(timer.current);
-			timer.current = null;
-			retryCount.current = 0;
-		}
-	}, []);
+  /* cancel polling */
+  const cancel = useMemoizedFn(() => {
+    if (timer.current) {
+      console.log('akira.timer.current :>> ', timer.current);
+      clearTimeout(timer.current);
+      timer.current = null;
+      console.log('akira.timer.current :>> ', timer.current);
+      retryCount.current = 0;
+    }
+  });
 
-	/* polling */
-	const _loop = useCallback((params?: TParams) => {
-		/* when polling retry count is -1, polling infinitely */
-		if (
-			pollingRetryCount !== -1 &&
-			pollingRetryCount &&
-			retryCount.current >= pollingRetryCount
-		) {
-			return;
-		}
-		retryCount.current++;
-		timer.current = setTimeout(() => {
-			run(params);
-		}, pollingInterval);
-	}, []);
+  /* polling */
+  const _loop = useMemoizedFn((params?: TParams) => {
+    /* when polling retry count is -1, polling infinitely */
+    if (pollingRetryCount && retryCount.current >= pollingRetryCount) return;
+    retryCount.current++;
+    timer.current = setTimeout(() => {
+      run(params);
+    }, pollingInterval);
+    console.log('akira._loop.timer.current :>> ', timer.current);
+  });
 
-	/* execute request */
-	const run = useCallback(async (params?: TParams) => {
-		try {
-			setLoading(true);
-			const res = await api(params);
-			setData(res);
-			setLoading(false);
-			if (pollingInterval) _loop(params);
-			return res;
-		} catch (err) {
-			setLoading(false);
-			console.error(err);
-		}
-	}, []);
+  /* execute request */
+  const run = useMemoizedFn(async (params?: TParams) => {
+    try {
+      setLoading(true);
+      const res = await api(params);
+      if (pollingInterval) _loop(params);
+      setData(res);
+      setLoading(false);
+      return res;
+    } catch (err) {
+      setLoading(false);
+      console.error(err);
+    }
+  });
 
-	/* manually trigger */
-	useEffect(() => {
-		if (!manual) run();
-	}, []);
+  /* manually trigger */
+  useEffect(() => {
+    if (!manual) run();
+  }, []);
 
-	return { data, loading, cancel, run };
+  return { data, loading, cancel, run };
 };
